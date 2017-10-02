a0ec96e76b7626cb068461813f594253d0b4f545
EskAere <chalas936@gmail.com>
2017-10-02 16:09:53 -0400

    Ajout de la structure CSommet

--- OutilHeightMap/OutilHeightMap/Main.cpp
+++ OutilHeightMap/OutilHeightMap/Main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <DirectXMath.h>
 
+using namespace DirectX;
 
 int main() {
 	int X, Y;
 	float Dx, Dy;
-	float 
+	float * P;
+	std::ifstream fichier("Entree.txt");
+
+	class ErrorLecture {};
+
+	if (fichier)
+	{
+
+	}
 
 
 
 }
+
+struct CSommet {
+	XMFLOAT4 position;
+	XMFLOAT4 normale;
+};
