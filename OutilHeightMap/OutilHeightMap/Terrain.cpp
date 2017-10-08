9018370c596d443b6534ed414e51288a218bd5c8
David Michel Donais <davidmicheldonais@gmail.com>
2017-10-08 11:48:37 -0400

    refactor du code affreux de getNormal

--- OutilHeightMap/OutilHeightMap/Terrain.cpp
+++ OutilHeightMap/OutilHeightMap/Terrain.cpp
@@ -1,15 +1,15 @@
 #include "Terrain.h"
 #include <algorithm>
 #include <iostream>
-#include <DirectXMath.h>
 
 using namespace std;
+using namespace DirectX;
 
 void Terrain::BuildVertices(const HeightMapFile& file) {
 	vertices.reserve(file.data.size());
 	int index{};
 	for_each(file.data.begin(), file.data.end(), [&](auto p) {
-		vertices.push_back({ GetPosition(info, index++, p), {} });
+		vertices.push_back({ GeneratePosition(index++, p), {} });
 	});
 }
 
@@ -31,11 +31,61 @@ void Terrain::BuildNormals()
 {
 	int index{};
 	for_each(vertices.begin(), vertices.end(), [&](Vertex& v) {
-		v.normal = GetNormal(info, index++);
+		v.normal = GenerateNormal(index++);
 	});
 }
 
-std::ostream& operator<<(std::ostream& os, const Terrain & t)
+XMVECTOR Terrain::GenerateNorthVector(int index) const
+{
+	return XMLoadFloat4(&vertices[index - info.X].position) - XMLoadFloat4(&vertices[index].position);
+}
+
+XMVECTOR Terrain::GenerateSouthVector(int index) const
+{
+	return XMLoadFloat4(&vertices[index + info.X].position) - XMLoadFloat4(&vertices[index].position);
+}
+
+XMVECTOR Terrain::GenerateEastVector(int index) const
+{
+	return XMLoadFloat4(&vertices[index + 1].position) - XMLoadFloat4(&vertices[index].position);
+}
+
+XMVECTOR Terrain::GenerateWestVector(int index) const
+{
+	return XMLoadFloat4(&vertices[index - 1].position) - XMLoadFloat4(&vertices[index].position);
+}
+
+XMFLOAT4 Terrain::GeneratePosition(int index, float z) const
+{
+	return { info.Dx * (index % info.X), info.Dy * (index / info.X), z, 1.0 };
+}
+
+XMFLOAT4 Terrain::GenerateNormal(int index) const
+{
+	auto edge = XMVectorSet(0, 0, 1, 0);
+	bool north = VertexHasNorth(index);
+	bool south = VertexHasSouth(index);
+	bool east = VertexHasEast(index);
+	bool west = VertexHasWest(index);
+
+	XMVECTOR v3 = north ? GenerateNorthVector(index) : XMVECTOR{};
+	XMVECTOR v1 = south ? GenerateSouthVector(index) : XMVECTOR{};
+	XMVECTOR v2 = east ? GenerateEastVector(index) : XMVECTOR{};
+	XMVECTOR v4 = west ? GenerateWestVector(index) : XMVECTOR{};
+
+	XMVECTOR n1 = south && east ? XMVector3Normalize(XMVector3Cross(v2, v1)) : edge;
+	XMVECTOR n2 = east && north ? XMVector3Normalize(XMVector3Cross(v3, v2)) : edge;
+	XMVECTOR n3 = north && west ? XMVector3Normalize(XMVector3Cross(v4, v3)) : edge;
+	XMVECTOR n4 = west && south ? XMVector3Normalize(XMVector3Cross(v1, v4)) : edge;
+
+	n1 = (n1 + n2 + n3 + n4) / 4;
+
+	XMFLOAT4 result;
+	XMStoreFloat4(&result, n1);
+	return result;
+}
+
+ostream& operator<<(ostream& os, const Terrain & t)
 {
 	if (os) {
 		os.write((char*)&t.GetInfo(), sizeof(TerrainFileHeader));
@@ -46,7 +96,7 @@ std::ostream& operator<<(std::ostream& os, const Terrain & t)
 	return os;
 }
 
-std::istream& operator>>(std::istream& is, TerrainFileHeader& t)
+istream& operator>>(istream& is, TerrainFileHeader& t)
 {
 	if (is) {
 		is >> t.X;
--- OutilHeightMap/OutilHeightMap/Terrain.h
+++ OutilHeightMap/OutilHeightMap/Terrain.h
@@ -36,37 +36,29 @@ private:
 	void BuildVertices(const HeightMapFile& file);
 	void BuildTriangles();
 	void BuildNormals();
-	static DirectX::XMFLOAT4 GetPosition(const TerrainFileHeader & info, int index, float z) {
-		return { info.Dx * (index % info.X), info.Dy * (index / info.X), z, 1.0 };
+
+	bool VertexHasNorth(int index) const
+	{
+		return index >= info.X;
 	}
-	DirectX::XMFLOAT4 GetNormal(const TerrainFileHeader & info, int index) {
-		using namespace DirectX;
-
-		XMVECTOR n1;
-		XMVECTOR n2;
-		XMVECTOR n3;
-		XMVECTOR n4;
-
-		XMVECTOR v1;
-		XMVECTOR v2;
-		XMVECTOR v3;
-		XMVECTOR v4;
-
-		n1 = n2 = n3 = n4 = DirectX::XMVectorSet(0, 0, 1, 0);
-		if (index < (info.X * info.Y) - info.X)  v1 = XMLoadFloat4(&vertices[index + info.X].position) - XMLoadFloat4(&vertices[index].position);
-		if (index%info.X != info.X - 1)			 v2 = XMLoadFloat4(&vertices[index + 1].position) - XMLoadFloat4(&vertices[index].position);
-		if (index >= info.X)					 v3 = XMLoadFloat4(&vertices[index - info.X].position) - XMLoadFloat4(&vertices[index].position);
-		if (index%info.X != 0)					 v4 = XMLoadFloat4(&vertices[index - 1].position) - XMLoadFloat4(&vertices[index].position);
-
-		if (index < (info.X * info.Y) - info.X    && index%info.X != info.X - 1)   n1 = XMVector3Cross(v2, v1);
-		if (index%info.X != info.X - 1		      && index >= info.X)			   n2 = XMVector3Cross(v3, v2);
-		if (index >= info.X					      && index%info.X != 0)			   n3 = XMVector3Cross(v4, v3);
-		if (index < (info.X * info.Y) - info.X    && index%info.X != 0)			   n4 = XMVector3Cross(v1, v4);
-
-		n1 = (XMVector3Normalize(n1) + XMVector3Normalize(n2) + XMVector3Normalize(n3) + XMVector3Normalize(n4)) / 4;
-
-		XMFLOAT4 resultat;
-		XMStoreFloat4(&resultat, n1);
-		return resultat;
+	bool VertexHasSouth(int index) const
+	{
+		return index < (info.X * info.Y) - info.X;
 	}
+	bool VertexHasEast(int index) const
+	{
+		return index%info.X != info.X - 1;
+	}
+	bool VertexHasWest(int index) const
+	{
+		return index%info.X != 0;
+	}
+
+	DirectX::XMVECTOR GenerateNorthVector(int index) const;
+	DirectX::XMVECTOR GenerateSouthVector(int index) const;
+	DirectX::XMVECTOR GenerateEastVector(int index) const;
+	DirectX::XMVECTOR GenerateWestVector(int index) const;
+
+	DirectX::XMFLOAT4 GeneratePosition(int index, float z) const;
+	DirectX::XMFLOAT4 GenerateNormal(int index) const;
 };
