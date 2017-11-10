3cd3bb9689435c1f97153f45dd026d86462abe4d
maevachr <32179501+maevachr@users.noreply.github.com>
2017-11-10 16:33:27 -0500

    Correction controle et vitesse camera dynamique

--- PetitMoteur3Dv6/PetitMoteur3D/DynamicCamera.cpp
+++ PetitMoteur3Dv6/PetitMoteur3D/DynamicCamera.cpp
@@ -29,8 +29,87 @@ namespace PM3D {
 		direction = XMVector4Normalize(direction_in);
 		up = XMVector4Normalize(up_in);
 		pGestionnaireDeSaisie = pGestionnaireDeSaisie_in;
-		right = XMVector3Cross(up, direction);
+		right = XMVector3Cross(direction, up);
+		vitesse_translation = VITESSE_T_DEFAULT;
+		vitesse_rotation = VITESSE_R_DEFAULT;
 	}
+	void CDynamicCamera::AnimeCamera(float tempsEcoule)
+	{
 
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_UP))
+		{
+			// Avancer
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(vitesse_translation * tempsEcoule * direction));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_DOWN))
+		{
+			// Reculer
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(-vitesse_translation * tempsEcoule * direction));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_LEFT))
+		{
+			// Rotation selon up à gauche
+
+			direction = XMVector4Transform(direction, XMMatrixRotationAxis(up, vitesse_rotation*tempsEcoule));
+			right = XMVector4Transform(right, XMMatrixRotationAxis(up, vitesse_rotation*tempsEcoule));
+
+			//FAUX FAIRE UNE ROTATION SUIVANT LE Y DE UP
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_RIGHT))
+		{
+			// Rotation selon up à droite
+			direction = XMVector4Transform(direction, XMMatrixRotationAxis(up, -vitesse_rotation*tempsEcoule));
+			right = XMVector4Transform(right, XMMatrixRotationAxis(up, -vitesse_rotation*tempsEcoule));
+			//FAUX FAIRE UNE ROTATION SUIVANT LE Y DE UP
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_PRIOR))
+		{
+			// Rotation selon right vers le haut
+			direction = XMVector4Transform(direction, XMMatrixRotationAxis(right, vitesse_rotation*tempsEcoule));
+			up = XMVector4Transform(up, XMMatrixRotationAxis(right, vitesse_rotation*tempsEcoule));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NEXT))
+		{
+			// Rotation selon right vers le bas
+			direction = XMVector4Transform(direction, XMMatrixRotationAxis(right, -vitesse_rotation*tempsEcoule));
+			up = XMVector4Transform(up, XMMatrixRotationAxis(right, -vitesse_rotation*tempsEcoule));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD4))
+		{
+			// Translation vers la gauche
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(-vitesse_translation * tempsEcoule *right));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD6))
+		{
+			// Translation vers la droite
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(vitesse_translation * tempsEcoule *right));
+
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD8))
+		{
+			// Translation vers le haut
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(vitesse_translation * tempsEcoule *up));
+
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD2))
+		{
+
+			// Translation vers le bas
+			position = XMVector4Transform(position, XMMatrixTranslationFromVector(-vitesse_translation * tempsEcoule *up));
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD1))
+		{
+			// Rotation selon direction vers la gauche
+			right = XMVector4Transform(right, XMMatrixRotationAxis(direction, -vitesse_rotation*tempsEcoule));
+			up = XMVector4Transform(up, XMMatrixRotationAxis(direction, -vitesse_rotation*tempsEcoule));
+
+		}
+		if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD3))
+		{
+			// Rotation selon direction vers la droite
+			right = XMVector4Transform(right, XMMatrixRotationAxis(direction, vitesse_rotation*tempsEcoule));
+			up = XMVector4Transform(up, XMMatrixRotationAxis(direction, vitesse_rotation*tempsEcoule));
+		}
+	}
 
 }
--- PetitMoteur3Dv6/PetitMoteur3D/DynamicCamera.h
+++ PetitMoteur3Dv6/PetitMoteur3D/DynamicCamera.h
@@ -11,8 +11,11 @@ namespace PM3D
 	class CDynamicCamera : public CCamera
 	{
 	protected:
-		float dvitesse_translation = 0.3f;
-		float dvitesse_rotation = 0.01f;
+		const float VITESSE_T_DEFAULT = 500.0f;
+		const float VITESSE_R_DEFAULT = XM_PIDIV2;
+
+		float vitesse_translation; // unit/sec
+		float vitesse_rotation; // rad/sec
 		CDIManipulateur* pGestionnaireDeSaisie;
 	public:
 		CDynamicCamera() {}
@@ -33,84 +36,7 @@ namespace PM3D
 			XMMATRIX* pMatViewProj_in,
 			CDIManipulateur* pGestionnaireDeSaisie_in);
 
-		virtual void AnimeCamera(float tempsEcoule) override
-		{
-
-			// Vérifier l'état de la touche gauche
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_UP))
-			{
-				// Avancer
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(10 * direction));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_DOWN))
-			{
-				// Reculer
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(-10 * direction));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_LEFT))
-			{
-				// Rotation selon up à gauche
-				direction = XMVector4Transform(direction, XMMatrixRotationAxis(up, (XM_PI * 2.0f) * -dvitesse_rotation));
-				right = XMVector4Transform(right, XMMatrixRotationAxis(up, (XM_PI * 2.0f) * -dvitesse_rotation));
-
-				//FAUX FAIRE UNE ROTATION SUIVANT LE Y DE UP
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_RIGHT))
-			{
-				// Rotation selon up à droite
-				direction = XMVector4Transform(direction, XMMatrixRotationAxis(up, (XM_PI * 2.0f) * dvitesse_rotation));
-				right = XMVector4Transform(right, XMMatrixRotationAxis(up, (XM_PI * 2.0f) * dvitesse_rotation));
-				//FAUX FAIRE UNE ROTATION SUIVANT LE Y DE UP
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_PRIOR))
-			{
-				// Rotation selon right vers le haut
-				direction = XMVector4Transform(direction, XMMatrixRotationAxis(right, (XM_PI * 2.0f) * -dvitesse_rotation));
-				up = XMVector4Transform(up, XMMatrixRotationAxis(right, (XM_PI * 2.0f) * -dvitesse_rotation));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NEXT))
-			{
-				// Rotation selon right vers le bas
-				direction = XMVector4Transform(direction, XMMatrixRotationAxis(right, (XM_PI * 2.0f) * dvitesse_rotation));
-				up = XMVector4Transform(up, XMMatrixRotationAxis(right, (XM_PI * 2.0f) * dvitesse_rotation));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD4))
-			{
-				// Translation vers la gauche
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(-right));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD6))
-			{
-				// Translation vers la droite
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(right));
-
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD8))
-			{
-				// Translation vers le haut
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(up));
-
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD2))
-			{
-
-				// Translation vers le bas
-				position = XMVector4Transform(position, XMMatrixTranslationFromVector(-up));
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD1))
-			{
-				// Rotation selon direction vers la gauche
-				right = XMVector4Transform(right, XMMatrixRotationAxis(direction, (XM_PI * 2.0f) * dvitesse_rotation));
-				up = XMVector4Transform(up, XMMatrixRotationAxis(direction, (XM_PI * 2.0f) * dvitesse_rotation));
-
-			}
-			if (pGestionnaireDeSaisie->ToucheAppuyee(DIK_NUMPAD3))
-			{
-				// Rotation selon direction vers la droite
-				right = XMVector4Transform(right, XMMatrixRotationAxis(direction, (XM_PI * 2.0f) * -dvitesse_rotation));
-				up = XMVector4Transform(up, XMMatrixRotationAxis(direction, (XM_PI * 2.0f) * -dvitesse_rotation));
-			}
-		}
+		virtual void AnimeCamera(float tempsEcoule) override;
 
 	};
 
