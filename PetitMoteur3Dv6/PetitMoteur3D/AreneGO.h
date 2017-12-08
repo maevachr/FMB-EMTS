237c6ae805c072bae074f1eaad25fff97d89214a
maevachr <32179501+maevachr@users.noreply.github.com>
2017-12-08 14:53:47 -0500

    Ajout de AreneGO

---
+++ PetitMoteur3Dv6/PetitMoteur3D/AreneGO.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameObject.h"
+namespace PM3D
+{
+	class AreneGO : public GameObject
+	{
+	private:
+		static constexpr char* typeId = "AreneGo";
+	public:
+
+		virtual void OnSpawn(const PxTransform& _transform = PxTransform::createIdentity(), GameObject* _parent = nullptr) override;
+		virtual void OnUnspawn() override;
+	public:
+		AreneGO();
+		~AreneGO();
+	};
+}
+
