1f2779242941a3c5621f211a33c8e5ed98048896
maevachr <32179501+maevachr@users.noreply.github.com>
2017-12-11 19:14:08 -0500

    Resize container

--- PetitMoteur3Dv6/PetitMoteur3D/SpawnManager.h
+++ PetitMoteur3Dv6/PetitMoteur3D/SpawnManager.h
@@ -39,105 +39,19 @@ namespace PM3D
 		std::vector<GameObject*> existingGameObjects;
 		std::vector<GameObject*> unspawnlist;
 	public:
-		void Init()
-		{
-			//Initialisation du Monde
+		void Init();
 
-			//Always put MonsterTruckGo first
-			PxTransform locationTruck = PxTransform::createIdentity();
-			locationTruck.p = PxVec3{ 5, 5, 3 };
-			Spawn<MonsterTruckGo>(locationTruck);
-
-			Spawn<SkyBoxGo>();
-			//Dernier objet à être rendu
-			Spawn<TerrainGo>();
-
-			PxTransform locationCrate = PxTransform::createIdentity();
-			locationCrate.p = PxVec3{ 20, 20, 6 };
-			Spawn<CrateGo<BrownCrate>>(locationCrate);
-
-			PxTransform locationCrate2 = PxTransform::createIdentity();
-			locationCrate2.p = PxVec3{ 40, 20, 6 };
-			Spawn<CrateGo<BrownCrate>>(locationCrate2);
-
-			PxTransform locationCrate3 = PxTransform::createIdentity();
-			locationCrate3.p = PxVec3{ 60, 20, 6 };
-			Spawn<CrateGo<BrownCrate>>(locationCrate3);
-
-			PxTransform locationCrate4 = PxTransform::createIdentity();
-			locationCrate4.p = PxVec3{ 80, 20, 6 };
-			Spawn<CrateGo<BrownCrate>>(locationCrate4);
-
-			PxTransform locationTunnel = PxTransform::createIdentity();
-			locationTunnel.p = PxVec3{ 100, 50, 6 };
-			Spawn<TunnelGo>(locationTunnel);
-
-// 			PxTransform explodedBox = PxTransform::createIdentity();
-// 			explodedBox.p = PxVec3{ 30, 20, 6 };
-// 			Spawn<ExplodedBox>(explodedBox);
-
-			PxTransform locationBus = PxTransform::createIdentity();
-			locationBus.p = PxVec3{ 10, 10, 6 };
-			Spawn<BusGo>(locationBus);
-
-			PxTransform locationCircusGo = PxTransform::createIdentity();
-			locationCircusGo.p = PxVec3{ -10, -10, 0 };
-			Spawn<CircusGo>(locationCircusGo);
-
-			PxTransform locationArene = PxTransform::createIdentity();
-			locationArene.p = PxVec3{ 0, 0, 8 };
-			//rotation de l'arene
-			Spawn<AreneGo>(locationArene);
-
-			PxTransform locationContainer = PxTransform::createIdentity();
-			locationContainer.p = PxVec3{ 20, -10, 5 };
-			//rotation de l'arene
-			Spawn<ContainerGo>(locationContainer);
-		}
-
-		void Update()
-		{
-			//unspawn objects in unspawnlist
-			for_each(begin(unspawnlist), end(unspawnlist), [&](GameObject* go) {
-				go->OnUnspawn();
-				delete go;
-			});
-			
-			unspawnlist.clear();
-		}
-		void CleanUp()
-		{
-			
-			for_each(begin(existingGameObjects), end(existingGameObjects), [&](GameObject* go) {
-				go->OnUnspawn();
-				delete go;
-			});
-			for_each(begin(unspawnlist), end(unspawnlist), [&](GameObject* go) {
-				go->OnUnspawn();
-				delete go;
-			});
-		}
+		void Update();
+		void CleanUp();
 
 		void AddGameObjects(GameObject* go) {
 			existingGameObjects.push_back(go);
 		}
 
 		template<class T>
-		void Spawn(const PxTransform& transform = PxTransform::createIdentity())
-		{
-			T* t = new T();
-			t->OnSpawn(transform);
-		}
+		void Spawn(const PxTransform& transform = PxTransform::createIdentity());
 		
-		void Unspawn(GameObject* go)
-		{
-			auto it = std::find(begin(existingGameObjects), end(existingGameObjects), go);
-			//Bloc unspawning of non existing object
-			if (it != existingGameObjects.end()) {
-				unspawnlist.push_back(*it);
-				existingGameObjects.erase(it);
-			}
-		}
+		void Unspawn(GameObject* go);
 
 		GameObject* GetPlayer()
 		{
@@ -148,4 +62,10 @@ namespace PM3D
 			return existingGameObjects;
 		}
 	};
+	template<class T>
+	inline void SpawnManager::Spawn(const PxTransform & transform)
+	{
+		T* t = new T();
+		t->OnSpawn(transform);
+	}
 }
