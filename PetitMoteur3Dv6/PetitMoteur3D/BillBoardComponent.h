b57b589705eadd7859176f4cfa266b1b934e96bc
David Michel Donais <David.Michel.Donais@USherbrooke.ca>
2017-12-13 16:30:09 -0500

    TV en place

--- PetitMoteur3Dv6/PetitMoteur3D/BillBoardComponent.h
+++ PetitMoteur3Dv6/PetitMoteur3D/BillBoardComponent.h
@@ -30,18 +30,20 @@ namespace PM3D
 
 	private:
 		BillBoard* b;
-		int index = 0;;
+		int index = 0;
+		float theta;
 	public:
 		BillBoard* GetBillBoard() { return b; }
 		void GetBillBoard(const char* name)
 		{
 			b = BillBoardManager::GetInstance().GetBillBoard(name);
 		}
+		void SetTheta(float _theta) { theta = _theta; }
 		//virtual void Anime();
 
 		virtual void Draw()
 		{
-			b->Draw(GetOwner()->GetPosition(), index++, GetOwner()->GetBreakingSpeed());
+			b->Draw(GetOwner(), index++, GetOwner()->GetBreakingSpeed(), theta);
 			if (index == b->GetFramesAmount()) index = 0;
 		}
 	};
